a67d39503dd89aea776b1c8eb5889c089349ac6b
pushkyn <alex.pushkyn@gmail.com>
2013-02-11 23:35:56 +0400

    Add tests for NotePresenter

---
+++ WinformsMVP.Tests/NotePresenterTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WinformsMVP.Model;
+using WinformsMVP.Presenter;
+using WinformsMVP.View;
+
+namespace WinformsMVP.Tests
+{
+    [TestClass]
+    public class NotePresenterTest
+    {
+        private readonly List<string> _notes = new List<String> {"One", "Two"};
+
+        [TestMethod]
+        public void CanLoadCustomers()
+        {
+            // arrange
+            var repo = new Mock<INoteRepository>();
+            var view = new Mock<INoteView>();
+            repo.Setup(m => m.NoteCards)
+                .Returns(_notes).Verifiable();
+
+            var presenter = new NotePresenter(view.Object, repo.Object);
+
+            // act
+
+            // assert
+            repo.Verify();
+            view.VerifySet(m => m.NotesList = _notes.ToList());
+        }
+    }
+}
